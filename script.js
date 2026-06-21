0977a6318893e07348d6f4496c95f053b27e88eb
Camila Silva <camillassantos32@gmail.com>
2026-06-21 16:46:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const cliente = {
+    nome: "Camila Santos",
+    segmento: "Premium",
+
+    scoreCredito: 780,
+    scoreSeguranca: 96,
+
+    rendaMensal: 7500,
+
+    limiteDisponivel: 18000,
+
+    investimentos: {
+        cdb: 20000,
+        tesouro: 10000,
+        fundo: 5000
+    },
+
+    creditoPreAprovado: 45000,
+
+    riscoFraude: "Baixo",
+
+    ultimaMovimentacao: "PIX Recebido R$ 1.250"
+};
